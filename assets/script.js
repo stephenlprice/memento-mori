b5f5d08131b3321914a5b84f98dade7fcd185023
Stephen Price <stephen_lprice@live.com>
2021-01-11 15:28:46 -0600

    days are color coded correctly and text input auto focuses on creation

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,8 @@ $(document).ready(function() {
         var click = false;
         var input = $('<input type="text" class="w-100">');
         if (!click) {
-            $(this).append(input);
+            $(this).append($(input));
+            $("input").focus();
         }
         $(this).off(event);
     });
@@ -28,19 +29,20 @@ $(document).ready(function() {
 // Writes hours from 9am to 5pm on the calendar page
 function writeDay() {
     for (var i = 9; i <= 17; i++) {
-        var hour = dayjs().hour(i).minute(0).second(0).format('hh A');
-        if (hour > dayjs()) {
+        var hour = dayjs().set('hour', i).hour();
+        var hourPretty = dayjs().hour(i).minute(0).second(0).format('hh A');
+        if (hour < dayjs().hour()) {
             $("tbody").append($(/*html*/`
                 <tr class="past">
-                    <th class="w-25" scope="row">${hour}</th>
+                    <th class="w-25" scope="row">${hourPretty}</th>
                     <td class="w-75"><span></span></td>
                 </tr>
             `));
         }
-        else if (hour === dayjs()) {
+        else if (hour === dayjs().hour()) {
             $("tbody").append($(/*html*/`
                 <tr class="present">
-                    <th class="w-25" scope="row">${hour}</th>
+                    <th class="w-25" scope="row">${hourPretty}</th>
                     <td class="w-75"><span></span></td>
                 </tr>
             `));
@@ -48,7 +50,7 @@ function writeDay() {
         else {
             $("tbody").append($(/*html*/`
                 <tr class="future">
-                    <th class="w-25" scope="row">${hour}</th>
+                    <th class="w-25" scope="row">${hourPretty}</th>
                     <td class="w-75"><span></span></td>
                 </tr>
             `));
