c9f03c59f8125dfe99e35a86077fb10e1e111467
Stephen Price <stephen_lprice@live.com>
2021-01-11 02:37:12 -0600

    calendar loads dynamically and added event listeners to input activities

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,60 @@
 //import dayjs from 'dayjs'
 dayjs().format()
 
-// variables and elements
-var today = dayjs().format('dddd, MMMM D, YYYY');
-var now = dayjs().format('h:mm:ss A');
-
-// Writes the current day to the top of the page
-$("#currentDay").text(today);
-
 // Outputs current time
-$("tbody th").text(dayjs().format('hh:mm A'));
+dayjs().format('hh:mm A');
 
+function init () {
+    // Writes the current day to the top of the page
+    $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'));
+    // Fill in the day with hours
+    writeDay();
+}
 
+$(document).ready(function() {
+
+    // Listens for a click to add a text input to enter a new activity for the selected hour
+    $("tbody td.w-75").on("click", function(event) {
+        var click = false;
+        var input = $('<input type="text" class="w-100">');
+        if (!click) {
+            $(this).append(input);
+        }
+        $(this).off(event);
+    });
+
+});
+
+// Writes hours from 9am to 5pm on the calendar page
 function writeDay() {
     for (var i = 9; i <= 17; i++) {
         var hour = dayjs().hour(i).minute(0).second(0).format('hh A');
-
-        $("tbody").append($(/*html*/`
-            <tr>
-                <th scope="row">${hour}</th>
-                <td></td>
-            </tr>
-        `));
+        if (hour > dayjs()) {
+            $("tbody").append($(/*html*/`
+                <tr class="past">
+                    <th class="w-25" scope="row">${hour}</th>
+                    <td class="w-75"><span></span></td>
+                </tr>
+            `));
+        }
+        else if (hour === dayjs()) {
+            $("tbody").append($(/*html*/`
+                <tr class="present">
+                    <th class="w-25" scope="row">${hour}</th>
+                    <td class="w-75"><span></span></td>
+                </tr>
+            `));
+        }
+        else {
+            $("tbody").append($(/*html*/`
+                <tr class="future">
+                    <th class="w-25" scope="row">${hour}</th>
+                    <td class="w-75"><span></span></td>
+                </tr>
+            `));
+        }   
     }
 }
 
 
-// function timeContext() {
-//     if (){
-
-//     }
-// }
+init();
